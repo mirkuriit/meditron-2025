65438802f700a28bfcb9e286d05326dd7b7a7ea7
mirkuriit <tot-kto-idet@yandex.ru>
2025-11-16 17:22:00 +0800

    Ьущц

--- backend/src/routers/report_router.py
+++ backend/src/routers/report_router.py
@@ -1,18 +1,27 @@
 from uuid import UUID
 
+from loguru import logger
+
 from fastapi import APIRouter, Depends
 from src.schema.patient_info_schema import PatientInfo
 from src.schema.reports_schema import TreatmentType, TumorDynamic, SurvivalMonthReport
-
+from src.ml.model import global_predictor
 
 router = APIRouter(prefix="/reports", tags=["reports"])
 
 
-@router.post("/survival_month", response_model=SurvivalMonthReport)
+trash_field = ["treatment", "stage"]
+
+
+@router.post("/survival_month")
 async def get_survival_month(
     user: PatientInfo,
-):
-    return SurvivalMonthReport(month=10)
+):  
+    user_dict = user.model_dump()
+    stage = int(user_dict["stage"])
+    user_dict = {k: v for k, v in user_dict.items() if k not in trash_field}
+    logger.debug(user_dict)
+    return global_predictor.predict(stage=stage, patient_data=user_dict)
 
 
 @router.post("/tumor_dynamic", response_model=TumorDynamic)
