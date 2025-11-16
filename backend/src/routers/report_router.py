b8acd499a15e06a878f699485e06734f4be3c56d
mirkuriit <tot-kto-idet@yandex.ru>
2025-11-16 18:30:12 +0800

    rewrite ml

--- backend/src/routers/report_router.py
+++ backend/src/routers/report_router.py
@@ -10,18 +10,17 @@ from src.ml.model import global_predictor
 router = APIRouter(prefix="/reports", tags=["reports"])
 
 
-trash_field = ["treatment", "stage"]
 
-
-@router.post("/survival_month")
+@router.post("/survival_month", response_model=SurvivalMonthReport)
 async def get_survival_month(
     user: PatientInfo,
 ):  
     user_dict = user.model_dump()
     stage = int(user_dict["stage"])
-    user_dict = {k: v for k, v in user_dict.items() if k not in trash_field}
-    logger.debug(user_dict)
-    return global_predictor.predict(stage=stage, patient_data=user_dict)
+    user_dict = {k: v for k, v in user_dict.items()} | {"treatment":"surgery_chemo"}
+    model_response = global_predictor.predict(stage=stage, patient_data=user_dict)
+
+    return SurvivalMonthReport(month=round(model_response.get("predicted_survival_months")))
 
 
 @router.post("/tumor_dynamic", response_model=TumorDynamic)
